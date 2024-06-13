dd947352f65e89582dd8cded8e46d143e6650a04
SIDIK21 <162548735+MuhammadSdk@users.noreply.github.com>
2024-06-13 14:55:02 +0700

    Add files via upload

---
+++ 22.11.4676_P 10/praktikum9.1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+Modul PHP Dasar (Pertemuan 9) - Pemrograman Web
+2 Universitas Amikom Yogyakarta - 2022
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Praktikum 9</title>
+</head>
+<body>
+ <?php
+ $txt = "Hello World";
+ $x = 5;
+ $y = 10.5;
+ $Prodi = "Sistem Informasi";
+ echo "Tampilkan variabel txt : $txt";
+ echo "<br>";
+ echo "$x <br>";
+ echo "$y <br>";
+ echo "Prodi di Fakultas Ilmu Komputer : $Prodi";
+ ?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
